efa51d5277b0d6464a8d639890b7a6c41502bd9f
Madhura Pawar <madhurapawar425@gmail.com>
2025-11-05 22:24:15 +0530

    Refactor BPSK modulation and demodulation code

--- 5_BPSK_DC.m
+++ 5_BPSK_DC.m
@@ -1,10 +1,10 @@
 clc; 
 clear all; 
 close all; 
-%GENERATE CARRIER SIGNAL 
+
 Tb=1; t=0:Tb/100:Tb;
  fc=2; c=sqrt(2/Tb)*sin(2*pi*fc*t); 
-%generate message signal
+
  N=8; 
 m=rand(1,N); 
 t1=0; t2=Tb; 
@@ -18,9 +18,9 @@ if m(i)>0.5
                m_s=-1*ones(1,length(t)); 
 end 
 message(i,:)=m_s; 
-%product of carrier and message signal 
+
 bpsk_sig(i,:)=c.*m_s; %
-%Plot the message and BPSK modulated signal 
+
 subplot(5,1,2);axis([0 N -2 2]);plot(t,message(i,:),'r');
  title('message signal(POLAR form)');xlabel('t--->');ylabel('m(t)'); 
 grid on; hold on; 
@@ -30,7 +30,7 @@ grid on; hold on;
 t1=t1+1.01; t2=t2+1.01; 
 end
 hold off
-%plot the input binary data and carrier signal 
+
 subplot(5,1,1);stem(m);
  title('binary data bits');xlabel('n--->');ylabel('b(n)'); 
 grid on;
@@ -38,14 +38,14 @@ grid on;
 title('carrier signal');xlabel('t--->');ylabel('c(t)'); 
 grid on;
 
-% BPSK Demodulation 
+
 t1=0;t2=Tb 
 for i=1:N 
 t=[t1:.01:t2]
 end
-%correlator
+
  x=sum(c.*bpsk_sig(i,:)); 
-%decision device 
+
 if x>0 
 demod(i)=1; 
 else 
@@ -53,3 +53,4 @@ demod(i)=0;
 end 
 t1=t1+1.01;
 subplot(5,1,5);plot(demod(i));
+
